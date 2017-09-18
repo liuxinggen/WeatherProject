54f5e307cf1e1da0d88cf8810bfc2540dee4f953
柳兴根 <1057224736@qq.com>
2017-09-18 16:11:37 +0800

    Initial commit

---
+++ app/src/main/java/com/gengen/weather/weatherproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gengen.weather.weatherproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
